7c755bce42d7b4a2aeb84bcd1b97ece5fb24a120
iyoung5 <iyoung5@qq.com>
2023-02-10 11:53:33 +0800

    init && dev package

---
+++ src/AdminServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+// +----------------------------------------------------------------------
+// | Copyright (c) 2015~2023 zx All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: iyoung5 <iyoung5@vip.qq.com>
+// +----------------------------------------------------------------------
+// | Created on: 2023/2/10 11:38
+// +----------------------------------------------------------------------
+declare (strict_types=1);
+
+namespace Zx\Admin;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+    }
+}
